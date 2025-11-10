b8efb4760130744166133ebf4a1067a271a52438
swirling-wind <yaozirui@outlook.com>
2025-11-10 15:10:19 +0800

    Test std::print's uninitialized variable problem

--- 1109.cpp
+++ 1109.cpp
@@ -1,17 +1,9 @@
-#include <concepts>
+#include <iostream>
 #include <print>
-#include <vector>
-
-template <typename Container>
-concept has_direct_references = requires(Container& c) {
-    { c[0] } -> std::same_as<typename Container::value_type&>;
-};
 
 int main() {
-    static_assert(has_direct_references<std::vector<int>>);    // 通过 -> 正确！
-    static_assert(has_direct_references<std::vector<double>>); // 通过 -> 正确！
-    static_assert(has_direct_references<std::vector<char>>);   // 通过 -> 正确！
-    static_assert(
-        !has_direct_references<std::vector<bool>>); // 应不通过 -> 正确！
-    std::print("\n\nHello world\n");
+    double i;
+    // std::cout << i; // ERROR: Variable 'i' is uninitialized when used  here
+    std::print("{}", i); // PASSED: Not even a warning ?!
+    std::print("\nHello World!");
 }
